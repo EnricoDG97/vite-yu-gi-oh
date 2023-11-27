515b750ec4015fd52225066400563475e1183ff3
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-11-27 18:40:43 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+ <div class="container">
+    ciao
+</div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
+
+
+
+</style>
