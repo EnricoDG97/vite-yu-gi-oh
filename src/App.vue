5316697219a20fea73e0b9b454ae173bcdb85f35
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-11-27 18:55:53 +0100

    feat: added axios get method from API when page is created, saved in store.js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script>
-import AppHeader from './components/AppHeader.vue';
+    import axios from "axios";
+    import { store } from "./store.js";
+    import AppHeader from './components/AppHeader.vue';
 
     export default {
-    components: { AppHeader }
-}
+        components: { AppHeader },
+        data() {
+            return {
+                store,
+            }
+        },
+        created() {
+            axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+            .then((resp) => {
+                this.store.cards = resp.data;
+            })
+        }
+    }
 </script>
 
 <template>
